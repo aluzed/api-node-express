ace0070ea257aa8d6c20b802c62543ec35d1077c
Alexandre PENOMBRE <alexandre.penombre+Github@gmail.com>
2018-06-18 18:11:01 +0200

    update fk

--- OC-api-03-project/models/post_categories.js
+++ OC-api-03-project/models/post_categories.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const PostCategoriesSchema = new mongoose.Schema({
   libelle: { type: String, required: true },
-  cree_par: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateurs' },
+  cree_par: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
   date_creation: { type: Date, default: Date.now },
   date_modification: { type: Date, default: Date.now }    
 });
